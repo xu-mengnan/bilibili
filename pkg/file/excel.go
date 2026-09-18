027144cae3064b0b89bc70632434d743b5a20370
xu_mengnan <36294047+xu-mengnan@users.noreply.github.com>
2026-09-19 01:42:19 +0800

    export: return Excel write and close errors instead of panicking

--- pkg/file/excel.go
+++ pkg/file/excel.go
@@ -1,63 +1,59 @@
 package file
 
 import (
+	"fmt"
+
 	"github.com/xuri/excelize/v2"
 )
 
-// WriteExcel 写入Excel文件
-func WriteExcel(data [][]string, filePath string) error {
+func WriteExcel(data [][]string, filePath string) (err error) {
 	f := excelize.NewFile()
 	defer func() {
-		if err := f.Close(); err != nil {
-			panic(err)
+		if closeErr := f.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("close Excel file: %w", closeErr)
 		}
 	}()
 
-	// 创建一个工作表
 	index, err := f.NewSheet("Sheet1")
 	if err != nil {
 		return err
 	}
 
-	// 写入数据
 	for i, row := range data {
 		for j, cell := range row {
 			cellName, err := excelize.CoordinatesToCellName(j+1, i+1)
 			if err != nil {
 				return err
 			}
-			f.SetCellValue("Sheet1", cellName, cell)
+			// SetCellValue with a string stores a string cell; formulas are only
+			// created through SetCellFormula, so comment text remains literal.
+			if err := f.SetCellValue("Sheet1", cellName, cell); err != nil {
+				return err
+			}
 		}
 	}
 
-	// 设置默认工作表
 	f.SetActiveSheet(index)
-
-	// 保存文件
 	if err := f.SaveAs(filePath); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-// ReadExcel 读取Excel文件
-func ReadExcel(filePath string) ([][]string, error) {
+func ReadExcel(filePath string) (rows [][]string, err error) {
 	f, err := excelize.OpenFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer func() {
-		if err := f.Close(); err != nil {
-			panic(err)
+		if closeErr := f.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("close Excel file: %w", closeErr)
 		}
 	}()
 
-	// 获取工作表中的所有行
-	rows, err := f.GetRows("Sheet1")
+	rows, err = f.GetRows("Sheet1")
 	if err != nil {
 		return nil, err
 	}
-
 	return rows, nil
 }
